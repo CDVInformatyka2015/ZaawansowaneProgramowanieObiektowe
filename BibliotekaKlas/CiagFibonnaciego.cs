3d7c22894a74d8a94585d5265a7446399bc79a6c
otlet <otlet@protonmail.com>
2016-12-10 08:53:34 +0100

    Ikonka :3

---
+++ BibliotekaKlas/CiagFibonnaciego.cs
@@ -0,0 +1,19 @@
+﻿namespace BibliotekaKlas
+{
+    class CiagFibonnaciego
+    {
+
+        public static void DodajInt(ref int n1, ref int n2, ref int result)
+        {
+            n1 = n2;
+            n2 = result;
+            result = n1 + n2;
+        }
+        public static void DodajString(ref string n1, ref string n2, ref string result)
+        {
+            n1 = n2;
+            n2 = result;
+            result = (int.Parse(n1) + int.Parse(n2)).ToString();
+        }
+    }
+}
