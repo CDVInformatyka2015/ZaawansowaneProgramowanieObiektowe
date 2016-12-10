5c8742d65626034f8a7a72f7bb3b928f4abb7dfd
otlet <otlet0@gmail.com>
2016-12-10 08:36:53 +0100

    Ciung fibonaczegio

---
+++ Fibonnaciego/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Fibonnaciego
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+            textBox2.Text = "1";
+            textBox3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                textBox3.Text = (int.Parse(textBox1.Text)+int.Parse(textBox2.Text)).ToString();
+                return;
+            }
+
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = textBox3.Text;
+            textBox3.Text = (int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "" || int.Parse(textBox3.Text) <= 0)
+            {
+                return;
+            }
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = textBox3.Text;
+            textBox3.Text = (int.Parse(textBox1.Text) - int.Parse(textBox2.Text)).ToString();
+        }
+    }
+}
